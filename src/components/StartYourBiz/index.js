24cb58a373b774085888a928f77ab90b75a1453e
David Ochoa <david.ochoa@avonusa.com>
2019-02-25 10:12:58 -0500

    adding start your business page

--- src/components/StartYourBiz/index.js
+++ src/components/StartYourBiz/index.js
@@ -1,20 +1,20 @@
 import React from "react"
-
+import Link from "gatsby-link"
 
 
 
 export default props => 
-<div class="grey-bg get-started-banner text-center mt-5 pt-3 pb-5 hidden-xs">
-  <div class="mt-5">
+<div className="grey-bg get-started-banner text-center mt-5 pt-3 pb-5 hidden-xs">
+  <div className="mt-5">
     <h3>
       <strong>Start your business today
       </strong>
     </h3>
   </div>
-  <div class="mt-5 pb-3 text-center">
-    <a href="#">
-      <button class="btn btn-black">Get Started
+  <div className="mt-5 pb-3 text-center">
+    <Link to="#">
+      <button className="btn btn-black">Get Started
       </button>
-    </a>
+    </Link>
   </div>
 </div>
