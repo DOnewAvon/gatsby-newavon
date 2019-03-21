b4609482c43defcf1d8aeb13198912f38033ea59
David Ochoa <david.ochoa@avonusa.com>
2019-03-21 15:51:40 -0400

    add page 4

---
+++ src/pages/page-4.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import Layout from "../components/Layout"
+
+const ForthPage = () => <Layout>
+  <section class="container container--page p-0">
+    <h1>Hi people</h1>
+    <p>Welcome to page 3</p>
+    <Link to="/">Go back to the homepage</Link>
+  </section>
+</Layout>
+export default ForthPage
